1f6b3fb68f922223e49bb805687c0c66eaf536f7
parksy0109 <mtbeauti@gmail.com>
2022-05-17 17:27:02 +0900

    로그 찍어놓기 utility 파이썬 코드 추가

--- daishinAPIService.py
+++ daishinAPIService.py
@@ -1,4 +1,5 @@
 import win32com.client
+import utilitys
 
 
 # 연결 확인하는 함수 return 값 1일 경우 연결된 상태
@@ -9,14 +10,16 @@ def checkconnecting():
 
 # 해당 인덱스의 종목 데이터를 반환
 def getdata(type, index):
+    utilitys.printlog("getdata", "type = " + utilitys.tostring(type) + " index = " + utilitys.tostring(index))
     instCpStockCode = win32com.client.Dispatch('CpUtil.CpStockCode')
     return instCpStockCode.GetData(type, index)
 
 
 # 주식 이름으로 해당 인덱스랑 종목 코드 검색하는 함수
 def findbystockname(stockname):
+    utilitys.printlog("findbystockname",stockname)
     instCpStockCode = win32com.client.Dispatch('CpUtil.CpStockCode')
     stock_count = instCpStockCode.GetCount()
     for i in range(0, stock_count):
-        if instCpStockCode.GetData(1, i) == 'NAVER':
-            return 'index = ' + i.__str__() + ' 종목 코드 = ' + instCpStockCode.GetData(0, i)
+        if instCpStockCode.GetData(1, i) == stockname:
+            return 'index = ' + utilitys.tostring(i) + ' 종목 코드 = ' + instCpStockCode.GetData(0, i)
---
+++ utilitys.py
@@ -0,0 +1,6 @@
+def printlog(functionname, parameter):
+    print("daishinAPIService." + functionname + " :: " + parameter)
+
+
+def tostring(number):
+    return number.__str__()
