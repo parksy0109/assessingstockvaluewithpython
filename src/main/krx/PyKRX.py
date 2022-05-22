3f32ea7e91a3845908372e68218666127cee85e3
parksy0109 <mtbeauti@gmail.com>
2022-05-22 13:09:28 +0900

    1. stock code convert Ko-String 2. 평균 PER, PBR 출력 3. 최근 PER PBR 출력 4. 날짜별 BPS , PER, PBR, EPS, DIV, DPS DataFrame으로 출력

--- src/main/krx/PyKRX.py
+++ src/main/krx/PyKRX.py
@@ -5,5 +5,22 @@ if __name__ == '__main__':
     startDate = '20220101'
     endDate = '20220522'
     stockCode = '005930'
-    df = stock.get_market_fundamental(startDate, endDate, stockCode)
+    df = stock.get_market_fundamental(startDate, endDate, stockCode, freq='d', name_display=True)
+    values = df.columns.values
+    perList = df['PER'].tolist()
+    pbrList = df['PBR'].tolist()
+
+    ticker_list = stock.get_market_ticker_list()
+
+    stockName: str
+
+    print(stock.get_market_ticker_name(stockCode))
+    print(startDate + " 부터 " + endDate + " 까지 평균 PER :: " + round((sum(perList) / len(perList)), 2).__str__())
+
+    print(startDate + " 부터 " + endDate + " 까지 평균 PBR :: " + round((sum(pbrList) / len(pbrList)), 2).__str__(),
+          end="\n\n")
+
+    print(endDate + " PER :: " + str(round(perList[-1], 2)))
+    print(endDate + " PBR :: " + str(round(pbrList[-1], 2)), end="\n\n")
+
     print(df)
